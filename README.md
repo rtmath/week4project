76289eff0e6a63bf4522c57f40c782e44fcc6d75
Ryan Mathisen <ryantmathisen@gmail.com>
2016-11-04 15:09:59 -0700

    add planet delivery image, display inline blocks for the verification screen to better align each item

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # Pizza Project
 
 ### Specs
+* User will be able to choose between Delivery and Carry Out
+ * If user selects delivery, they will be prompted to enter their address
+  * If user selects Carry Out, they will be prompted to choose the location they would like to pick their pizza up from
 * User will be able to choose toppings and size for the pizza they'd like to order
 * Cost of pizza will depend on how many toppings the user chooses
 * User will have option between delivery and carryout
  * If delivery, user will be prompted to enter their address and information
  * If carryout, user will be able to skip this step and choose their desired located for pick up
-
-Pizza Options:
-Dough: Hand tossed pizza // Gluten free
-Size: Medium // Large // Family size
-Sauce: Marinara // Tomato // Buffalo
-Cheese: cheddar, american, pepperjack
-Meats: Pepperoni, Italian Sausage, Salami, Meatball, Canadian background-color
-Veggies:
-Mushrooms, onions, olives, peppers, pineapple, tomato
--- js/scripts.js
+++ js/scripts.js
@@ -1,112 +1,3 @@
-$('document').ready(function() {
-
-  $('#delivery').click(function(event) {
-    event.preventDefault();
-    $('#carryoutSelection').hide();
-    $('#pizzaSelection').hide();
-    $('#shoppingCart').hide();
-    $('#thankYou').hide();
-    $('#deliverySelection').fadeIn();
-    resetForms();
-  })
-
-  $('#carryout').click(function(event) {
-    event.preventDefault();
-    $('#deliverySelection').hide();
-    $('#pizzaSelection').hide();
-    $('#shoppingCart').hide();
-    $('#thankYou').hide();
-    $('#carryoutSelection').fadeIn();
-    resetForms();
-  })
-
-  $('#carryoutForm').submit(function(event) {
-    event.preventDefault();
-    var location = $('#locationSelect').val();
-    $('#pizzaSelection').fadeIn();
-    $('#address1').text("This order is for carryout.");
-    $('#address2').text("You can pick up your pizza from the following location:");
-    if (location === location1.locationName) {
-      $('#address3').text(location1.locationName);
-      $('#address4').text("Address: " + location1.address);
-      $('#address5').text("City: " + location1.city);
-      console.log("location1");
-    } else if (location === location2.locationName) {
-      $('#address3').text(location2.locationName);
-      $('#address4').text("Address: " + location2.address);
-      $('#address5').text("City: " + location2.city);
-      console.log("location2");
-    } else if (location === location3.locationName) {
-      $('#address3').text(location3.locationName);
-      $('#address4').text("Address: " + location3.address);
-      $('#address5').text("City: " + location3.city);
-      console.log("location1");
-    }
-    $('#carryoutSelection').hide();
-  });
-
-  $('#deliveryForm').submit(function(event) {
-    event.preventDefault();
-    $('#deliverySelection').hide();
-    $('#pizzaSelection').fadeIn();
-    var userAddress = new Address(
-      $('#formname').val(),
-      $('#formaddress1').val(),
-      $('#formaddress2').val(),
-      $('#formcity').val(),
-      $('#formstate').val(),
-      $('#formzip').val()
-    );
-    $('#address1').text("Name: " + userAddress.userName);
-    $('#address2').text("Address: " +userAddress.address1);
-    $('#address3').text("         " +userAddress.address2);
-    $('#address4').text("City: " + userAddress.city);
-    $('#address5').text("State: " + userAddress.state);
-    $('#address6').text("Zip: " + userAddress.zip);
-  });
-
-  $('#pizzaSelection').submit(function(event) {
-    event.preventDefault();
-    $('#pizzaSelection').hide();
-    var meats = [];
-   $('input:checkbox[name=meat]:checked').each(function() {
-     meats.push($(this).val());
-   })
-    var veggies = [];
-    $('input:checkbox[name=veggie]:checked').each(function() {
-      veggies.push($(this).val());
-    })
-    var userPizza = new Pizza(
-      $('#doughSelect').val(),
-      $('#sizeSelect').val(),
-      $('#sauceSelect').val(),
-      $('#cheeseSelect').val(),
-      meats,
-      veggies
-    )
-    $('#pizza1').text(userPizza.dough);
-    $('#pizza2').text(userPizza.pizzaSize);
-    $('#pizza3').text(userPizza.sauce);
-    $('#pizza4').text(userPizza.cheese);
-    $('#pizza5').text(userPizza.meats);
-    $('#pizza6').text(userPizza.veggies);
-    $('#finalCost').text(userPizza.cost());
-    $('#shoppingCart').fadeIn();
-  });
-
-  $('#orderEdit').click(function(event) {
-    event.preventDefault();
-
-    $('#pizzaSelection').fadeIn();
-    $('#shoppingCart').hide();
-  });
-
-  $('#finalizeOrder').submit(function(event) {
-    event.preventDefault();
-    $('#thankYou').fadeIn();
-    $('#shoppingCart').hide();
-  })
-});
 
 function Address (userName, address1, address2, city, state, zip) {
   this.userName = userName;
@@ -160,3 +51,129 @@ function resetForms() {
 var location1 = new Location("Spaceport VII", "Phi Sector", "Gamma Cluster");
 var location2 = new Location("Spaceport IV", "Galactic Way", "Lambda Cluster");
 var location3 = new Location("Satellite Theta MkII", "Tau Ypsilon", "Milky Way");
+
+$('document').ready(function() {
+
+  $('#delivery').click(function(event) {
+    event.preventDefault();
+    $('#carryoutSelection').hide();
+    $('#pizzaSelection').hide();
+    $('#shoppingCart').hide();
+    $('#thankYou').hide();
+    $('#deliverySelection').fadeIn();
+    resetForms();
+  })
+
+  $('#carryout').click(function(event) {
+    event.preventDefault();
+    $('#deliverySelection').hide();
+    $('#pizzaSelection').hide();
+    $('#shoppingCart').hide();
+    $('#thankYou').hide();
+    $('#carryoutSelection').fadeIn();
+    resetForms();
+  })
+
+  $('#carryoutForm').submit(function(event) {
+    event.preventDefault();
+    var location = $('#locationSelect').val();
+    $('#pizzaSelection').fadeIn();
+    $('#addressHeader').text("Carry Out:");
+    $('#carryout1').text("This order is for carryout.");
+    $('#carryout2').text("You can pick up your pizza from the following location:");
+    $('#addressTitle1').text("");
+    $('#addressTitle2').text("Location: ");
+    $('#addressTitle3').text("Address: ");
+    $('#addressTitle4').text("City: ");
+    $('#addressTitle5').text("");
+    $('#addressTitle6').text("");
+    $('#address1').text("");
+    $('#address5').text("");
+    $('#address6').text("");
+    if (location === location1.locationName) {
+      $('#address2').text(location1.locationName);
+      $('#address3').text(location1.address);
+      $('#address4').text(location1.town);
+    } else if (location === location2.locationName) {
+      $('#address2').text(location2.locationName);
+      $('#address3').text(location2.address);
+      $('#address4').text(location2.town);
+    } else if (location === location3.locationName) {
+      $('#address2').text(location3.locationName);
+      $('#address3').text(location3.address);
+      $('#address4').text(location3.town);
+    }
+    $('#carryoutSelection').hide();
+  });
+
+  $('#deliveryForm').submit(function(event) {
+    event.preventDefault();
+    $('#deliverySelection').hide();
+    $('#pizzaSelection').fadeIn();
+    var userAddress = new Address(
+      $('#formname').val(),
+      $('#formaddress1').val(),
+      $('#formaddress2').val(),
+      $('#formcity').val(),
+      $('#formstate').val(),
+      $('#formzip').val()
+    );
+    $('#carryout1').text("");
+    $('#carryout2').text("");
+    $('#addressHeader').text("Address");
+    $('#address1').text(userAddress.userName);
+    $('#address2').text(userAddress.address1);
+    $('#address3').text(userAddress.address2);
+    $('#address4').text(userAddress.city);
+    $('#address5').text(userAddress.state);
+    $('#address6').text(userAddress.zip);
+    $('#addressTitle1').text("Name: ");
+    $('#addressTitle2').text("Address: ");
+    $('#addressTitle3').text(" ");
+    $('#addressTitle4').text("City: ");
+    $('#addressTitle5').text("State: ");
+    $('#addressTitle6').text("Zip: ");
+  });
+
+  $('#pizzaSelection').submit(function(event) {
+    event.preventDefault();
+    $('#pizzaSelection').hide();
+    var meats = [];
+   $('input:checkbox[name=meat]:checked').each(function() {
+     meats.push($(this).val());
+   })
+    var veggies = [];
+    $('input:checkbox[name=veggie]:checked').each(function() {
+      veggies.push($(this).val());
+    })
+    var userPizza = new Pizza(
+      $('#doughSelect').val(),
+      $('#sizeSelect').val(),
+      $('#sauceSelect').val(),
+      $('#cheeseSelect').val(),
+      meats,
+      veggies
+    )
+    $('#pizza1').text(userPizza.dough);
+    $('#pizza2').text(userPizza.pizzaSize);
+    $('#pizza3').text(userPizza.sauce);
+    $('#pizza4').text(userPizza.cheese);
+    $('#pizza5').text(userPizza.meats);
+    $('#pizza6').text(userPizza.veggies);
+    $('#finalCost').text(userPizza.cost());
+    $('#shoppingCart').fadeIn();
+  });
+
+  $('#orderEdit').click(function(event) {
+    event.preventDefault();
+
+    $('#pizzaSelection').fadeIn();
+    $('#shoppingCart').hide();
+  });
+
+  $('#finalizeOrder').submit(function(event) {
+    event.preventDefault();
+    $('#thankYou').fadeIn();
+    $('#shoppingCart').hide();
+  })
+});
