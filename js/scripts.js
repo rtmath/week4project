f9c905478aac9a708647136bb89178697b2f7be1
Ryan Mathisen <ryantmathisen@gmail.com>
2016-11-04 13:11:43 -0700

    add alien pictures in order confirmation and thank you pages, add three pizza planet locations to carry out list and functionality to display the selected location's address in the confirmation screen for carryout

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,52 @@ $('document').ready(function() {
   $('#delivery').click(function(event) {
     event.preventDefault();
     $('#carryoutSelection').hide();
-    $('#deliverySelection').toggle();
+    $('#pizzaSelection').hide();
+    $('#shoppingCart').hide();
+    $('#thankYou').hide();
+    $('#deliverySelection').fadeIn();
+    resetForms();
   })
 
   $('#carryout').click(function(event) {
     event.preventDefault();
     $('#deliverySelection').hide();
-    $('#carryoutSelection').toggle();
+    $('#pizzaSelection').hide();
+    $('#shoppingCart').hide();
+    $('#thankYou').hide();
+    $('#carryoutSelection').fadeIn();
+    resetForms();
   })
 
   $('#carryoutForm').submit(function(event) {
     event.preventDefault();
     var location = $('#locationSelect').val();
-    $('#pizzaSelection').show();
+    $('#pizzaSelection').fadeIn();
     $('#address1').text("This order is for carryout.");
     $('#address2').text("You can pick up your pizza from the following location:");
-    $('#address3').text(location);
+    if (location === location1.locationName) {
+      $('#address3').text(location1.locationName);
+      $('#address4').text("Address: " + location1.address);
+      $('#address5').text("City: " + location1.city);
+      console.log("location1");
+    } else if (location === location2.locationName) {
+      $('#address3').text(location2.locationName);
+      $('#address4').text("Address: " + location2.address);
+      $('#address5').text("City: " + location2.city);
+      console.log("location2");
+    } else if (location === location3.locationName) {
+      $('#address3').text(location3.locationName);
+      $('#address4').text("Address: " + location3.address);
+      $('#address5').text("City: " + location3.city);
+      console.log("location1");
+    }
+    $('#carryoutSelection').hide();
   });
 
   $('#deliveryForm').submit(function(event) {
     event.preventDefault();
-    $('#orderSelection').hide();
     $('#deliverySelection').hide();
-    $('#pizzaSelection').show();
+    $('#pizzaSelection').fadeIn();
     var userAddress = new Address(
       $('#formname').val(),
       $('#formaddress1').val(),
@@ -34,12 +57,12 @@ $('document').ready(function() {
       $('#formstate').val(),
       $('#formzip').val()
     );
-    $('#address1').text(userAddress.userName);
-    $('#address2').text(userAddress.address1);
-    $('#address3').text(userAddress.address2);
-    $('#address4').text(userAddress.city);
-    $('#address5').text(userAddress.state);
-    $('#address6').text(userAddress.zip);
+    $('#address1').text("Name: " + userAddress.userName);
+    $('#address2').text("Address: " +userAddress.address1);
+    $('#address3').text("         " +userAddress.address2);
+    $('#address4').text("City: " + userAddress.city);
+    $('#address5').text("State: " + userAddress.state);
+    $('#address6').text("Zip: " + userAddress.zip);
   });
 
   $('#pizzaSelection').submit(function(event) {
@@ -68,20 +91,20 @@ $('document').ready(function() {
     $('#pizza5').text(userPizza.meats);
     $('#pizza6').text(userPizza.veggies);
     $('#finalCost').text(userPizza.cost());
-    $('#shoppingCart').show();
+    $('#shoppingCart').fadeIn();
   });
 
   $('#orderEdit').click(function(event) {
     event.preventDefault();
 
-    $('#pizzaSelection').show();
+    $('#pizzaSelection').fadeIn();
     $('#shoppingCart').hide();
-    console.log("Done!");
   });
 
   $('#finalizeOrder').submit(function(event) {
     event.preventDefault();
-    $('#thankYou').show();
+    $('#thankYou').fadeIn();
+    $('#shoppingCart').hide();
   })
 });
 
@@ -104,6 +127,12 @@ function Pizza (dough, pizzaSize, sauce, cheese, meats, veggies) {
   var toppings = this.meats.length;
 }
 
+function Location (locationName, address, town) {
+  this.locationName = locationName;
+  this.address = address;
+  this.town = town;
+}
+
 function chainLocation (address, city, state, zip) {
   this.address = address;
   this.city = city;
@@ -121,3 +150,13 @@ Pizza.prototype.cost = function() {
   }
   return Math.round(baseCost);
 }
+
+function resetForms() {
+  for (i = 0; i < document.forms.length; i++) {
+    document.forms[i].reset();
+  }
+}
+
+var location1 = new Location("Spaceport VII", "Phi Sector", "Gamma Cluster");
+var location2 = new Location("Spaceport IV", "Galactic Way", "Lambda Cluster");
+var location3 = new Location("Satellite Theta MkII", "Tau Ypsilon", "Milky Way");
