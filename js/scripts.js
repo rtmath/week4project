d92f23ce1a66cdb0140a3f04aa190f86ec6e80ae
Ryan Mathisen <ryantmathisen@gmail.com>
2016-11-04 10:55:07 -0700

    add additional form validation, shopping cart with order verification

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,72 @@
 $('document').ready(function() {
+
   $('#delivery').click(function(event) {
     event.preventDefault();
     $('#carryoutSelection').hide();
     $('#deliverySelection').toggle();
   })
+
   $('#carryout').click(function(event) {
     event.preventDefault();
     $('#deliverySelection').hide();
     $('#carryoutSelection').toggle();
   })
+
+  $('#carryoutForm').submit(function(event) {
+    event.preventDefault();
+    var location = $('#locationSelect').val();
+    $('#pizzaSelection').show();
+    $('#address1').text("This order is for carryout.");
+    $('#address2').text("You can pick up your pizza from the following location:");
+    $('#address3').text(location);
+  });
+
   $('#deliveryForm').submit(function(event) {
     event.preventDefault();
     $('#orderSelection').hide();
     $('#deliverySelection').hide();
     $('#pizzaSelection').show();
-    // var userAddress = new Address(
-    //   $('#formname').val(),
-    //   $('#formaddress1').val(),
-    //   $('#formaddress2').val(),
-    //   $('#formcity').val(),
-    //   $('#formstate').val(),
-    //   $('#formzip').val()
-    // );
-    // $('#output1').text(userAddress.userName);
-    // $('#output2').text(userAddress.address1);
-    // $('#output3').text(userAddress.address2);
-    // $('#output4').text(userAddress.city);
-    // $('#output5').text(userAddress.state);
-    // $('#output6').text(userAddress.zip);
+    var userAddress = new Address(
+      $('#formname').val(),
+      $('#formaddress1').val(),
+      $('#formaddress2').val(),
+      $('#formcity').val(),
+      $('#formstate').val(),
+      $('#formzip').val()
+    );
+    $('#address1').text(userAddress.userName);
+    $('#address2').text(userAddress.address1);
+    $('#address3').text(userAddress.address2);
+    $('#address4').text(userAddress.city);
+    $('#address5').text(userAddress.state);
+    $('#address6').text(userAddress.zip);
+  });
+
+  $('#pizzaSelection').submit(function(event) {
+    event.preventDefault();
+    var meats = [];
+   $('input:checkbox[name=meat]:checked').each(function() {
+     meats.push($(this).val());
+   })
+    var veggies = [];
+    $('input:checkbox[name=veggie]:checked').each(function() {
+      veggies.push($(this).val());
+    })
+    var userPizza = new Pizza(
+      $('#doughSelect').val(),
+      $('#sizeSelect').val(),
+      $('#sauceSelect').val(),
+      $('#cheeseSelect').val(),
+      meats,
+      veggies
+    )
+    $('#pizza1').text(userPizza.dough);
+    $('#pizza2').text(userPizza.pizzaSize);
+    $('#pizza3').text(userPizza.sauce);
+    $('#pizza4').text(userPizza.cheese);
+    $('#pizza5').text(userPizza.meats);
+    $('#pizza6').text(userPizza.veggies);
+    $('#shoppingCart').show();
   });
 });
 
@@ -47,6 +86,13 @@ function Pizza (dough, pizzaSize, sauce, cheese, meats, veggies) {
   this.cheese = cheese;
   this.meats = meats;
   this.veggies = veggies;
+}
+
+function chainLocation (address, city, state, zip) {
+  this.address = address;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
 }
 
 Pizza.prototype.cost = function() {
